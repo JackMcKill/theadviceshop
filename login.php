c4c05869a6830eb09f3a19dea1eb2c28be80ff9d
Jack McGill <mr.mcgilljh@gmail.com>
2021-05-03 14:05:22 +1000

    Demonstrate: Repitions & Functions

--- login.php
+++ login.php
@@ -1,7 +1,10 @@
 <?php
 error_reporting(E_ALL);
 session_start();
-$_SESSION['username'] = $_REQUEST['username'];
+$_SESSION['username'] = $_POST['name'];
+$_SESSION['password'] = $_POST['password'];
+$_SESSION['location'] = "Cairns";
 //echo $_GET['page'];
 header("Location: index.php");
+
 exit();
